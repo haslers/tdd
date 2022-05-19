c1f8c18684e38172772625d43df63e4bd66cc5e5
Stefanie Hasler <stefanie.hasler@codecentric.de>
2022-05-19 17:36:35 +0200

    first commit

---
+++ src/MarsRoverTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarsRoverTest {
+
+    @Test
+    public void shouldCompile() {
+        MarsRover marsRover = new MarsRover();
+    }
+}
